e96a0600ff25911e9969c1f276889246db8e591e
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-11 10:08:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function decimalToBinary(num) {
+
+  let binary ="";
+
+
+	while(num > 0){
+		let rem = num % 2;
+		binary += rem;
+		num = num / 2;
+		
+	}
+	let res = "";
+	for(let i = binary.length-1;i>=0;i--){
+		res += binary[i];
+	}
+	return Number(res);
   //Write you code here
   
 }
