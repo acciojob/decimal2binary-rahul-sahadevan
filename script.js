654a78fd79ebbf38870ec3cbe827a45f37fa1711
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-12 14:58:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function decimalToBinary(num) {
 
     let rem = 0;
-	let sum = "";
-	
-	while(num > 0){
-		rem = num % 2;
-		sum += rem;
-		num = parseInt(num/2);
-	}
-	let res = (sum.split('').reverse().join(''));
-	return Number(res);
+    let sum = "";
+
+    while (num > 0) {
+        rem = num % 2;
+        sum = rem + sum; // Corrected line
+        num = Math.floor(num / 2);
+    }
+
+    return sum;
 	  //Write you code here
 	  
 }
